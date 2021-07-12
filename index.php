c542dc39fcd1227529284e55069e9c90ad949a83
AmmarAsiri <86685327+AmmarAsiri@users.noreply.github.com>
2021-07-12 03:38:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $f = "FORWARD";
+        $b = "BACKWARD";
+        $l = "LEFT";
+        $r = "RIGHT";
+        $s = "STOP";
+
+        $host = 'localhost';
+        $dbusername = 'root';
+        $dbpassword = '';
+        $dbname = 'robot_base';
+
+        $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+        if (mysqli_connect_error()) {
+            die('connect error (' . mysqli_connect_errno() . ')' . mysqli_connect_error());
+        } else {
+            if (isset($_POST['f'])) {
+                $query = "INSERT INTO orders VALUES('$f')";
+                $run = mysqli_query($conn, $query);
+                echo 'forward';
+            } elseif (isset($_POST['b'])) {
+                $query = "INSERT INTO orders VALUES('$b')";
+                $run = mysqli_query($conn, $query);
+                echo 'backward';
+            } elseif (isset($_POST['r'])) {
+                $query = "INSERT INTO orders VALUES('$r')";
+                $run = mysqli_query($conn, $query);
+                echo 'right';
+            } elseif (isset($_POST['l'])) {
+                $query = "INSERT INTO orders VALUES('$l')";
+                $run = mysqli_query($conn, $query);
+                echo 'left';
+            } elseif (isset($_POST['s'])) {
+                $query = "INSERT INTO orders VALUES('$s')";
+                $run = mysqli_query($conn, $query);
+                echo 'stop';
+            }
+        }
+        ?>
+
+    </body>
+</html>
